5caa918836ad434d109076533e37bc5ed73f5d97
wilkgillian <wilkrosa1998@gmail.com>
2023-02-23 17:03:12 -0400

    .

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -4,38 +4,36 @@ import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
 import { useRouter } from 'next/router';
 
-interface SubscribeButtonProps {
-    priceId:string
-}
-export function SubscribeButton({priceId}: SubscribeButtonProps) {
-    const {data: session} = useSession();
-    const router = useRouter();
-    async function handleSubscribe(){
-        if(!session){
-            signIn('github');
-            return;
-        }
+export function SubscribeButton() {
+  const { data: session } = useSession();
+  const router = useRouter();
 
-        if(session.activeSubscription){
-            router.push('/posts');
-            return;
-        }
-
-        try {
-            const response = await api.post('/subscribe');
-            const { sessionId } = response.data;
-
-            const stripe = await getStripeJs();
-
-            await stripe.redirectToCheckout({sessionId});
-        } catch(err) {
-            alert(err.message);
-        }
+  async function handleSubscribe() {
+    if (!session) {
+      signIn('github');
+      return;
     }
 
-    return(
-        <button onClick={handleSubscribe}className={styles.subscribeButton}>
-            Subscribe now
-        </button>
-    )
+    if (session.activeSubscription) {
+      router.push('/posts');
+      return;
+    }
+
+    try {
+      const response = await api.post('/subscribe');
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (err) {
+      alert(err.message);
+    }
+  }
+
+  return (
+    <button onClick={handleSubscribe} className={styles.subscribeButton}>
+      Subscribe now
+    </button>
+  );
 }
